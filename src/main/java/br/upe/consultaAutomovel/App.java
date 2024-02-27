6c203b2a074e9a905c0e69fa1616f7507d2282cd
Daniel Lucena <daniellucena.jt@gmail.com>
2024-02-27 00:49:35 -0300

    Consulta básica

--- src/main/java/br/upe/consultaAutomovel/App.java
+++ src/main/java/br/upe/consultaAutomovel/App.java
@@ -1,7 +1,13 @@
 package br.upe.consultaAutomovel;
 
+import java.util.List;
+
+import javax.lang.model.SourceVersion;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+import org.hibernate.cache.spi.QueryResultsCache;
 
 import br.upe.consultaAutomovel.models.Automovel;
 import br.upe.consultaAutomovel.utils.JPAUtil;
@@ -14,6 +20,17 @@ public class App {
         auto.setAnoFabricacao(2016);
 
         EntityManager em = JPAUtil.getEntityManager();
+
+        Query q = em.createQuery("select a from Automovel a", Automovel.class);
+
+        List<Automovel> autos = q.getResultList();
+        
+        for(Automovel a : autos)
+        {
+            System.out.println(a.getMarca());
+            System.out.println(a.getModelo());
+        }
+
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         em.persist(auto);
